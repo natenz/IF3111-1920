407fb8b569fe05bdcb708ae7c3d3af538a29e0ed
nrmynitasr <49836876+nrmynitasr@users.noreply.github.com>
2019-12-03 22:02:17 +0700

    tampilan 1 awal

---
+++ application/views/tampilan1.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lapor ITERA</title>
+    <link rel="stylesheet" type="text/css" href="asset/css/style.css">
+</head>
+
+<body>
+    <div class="header">
+        <h1 style="text-align: center;">LAPOR ITERA!</h1>
+    </div>
+    <div id="search-box">
+        <form action="/search" id="search-form" method="get" target="_top">
+            <input id="search-text" name="keyword" placeholder="Search here..." type="text" />
+            <button id="search-button" type="submit" name="cari">Search</button>
+        </form>
+    </div>
+    <div class="create">
+        <h6>Buat Laporan/Komentar
+            <a href=""><img src="asset/img/add.png" width="25px" height="20px"></a>
+        </h6>
+    </div>
+    <div class="comment">
+        <h5>Laporan/Komentar Terakhir
+            <hr>
+        </h5>
+        <p> Saya salah satu warga kota tangerang no hp. 081411022970 ingin melaporkan kepada pemda
+            kota tangerang (untuk diteruskan kepada pihak yang berwenang)tentang kondisi pohon besar
+            dan tinggi yang berlokasi di jalan raden fatah tepatnya didepan sdn 01, 10, 12 sudimara barat
+            dan lokasinya juga
+        </p>
+        <h5>Lampiran: <a style="text-align: right;">Waktu : 20-12-2019 20:20</a> <a href="">Lihat Selengkapnya ></a>
+            <hr>
+        </h5>
+        <p>Selamat Siang,
+            Saya ingin menanyakan perihal antrian paspor online, tanggal 09/11/2018 saya mendaftarkan akun saya melalui
+            web, ketika
+            saya mau mengakses lagi tahun ini, saya mencoba masuk via aplikasi "layanan paspor online" dan via website,
+            email saya
+            tidak dikenali dan dsuruh registrasi baru. Saat saya registrasi ulang, NIK saya telah dipakai.
+            ketika saya lihat di internet, sarannya menggunakan NIK keluarga atau saudara yang kemungkinan tidak membuat
+            paspor
+            dalam waktu dekat. Apa tidak ada solusi lain? saya tidak ingin membuat saudara saya susah membuat paspor
+            kedepannya
+            nanti.
+            bisa tolong bantuannya? saya akan pergi keluar bulan maret 2020.
+            terimakasih banyak.</p>
+        <h5>Lampiran: <a style="text-align: right;">Waktu : 20-12-2019 20:20</a> <a href="">Lihat Selengkapnya ></a>
+            <hr>
+        </h5>
+        <p>Selamat sore saya ingin meminta kejelasan atas berkas yang saya daftarkan prona ptsl perseorangan bukan
+            melalui rt
+            setempat agustus 2018 di wilayah duren sawit rt 008/001,awal januari 2019 saya dapat kabar katanya akan ada
+            pencetakan
+            sertifikat di bulan maret 2019 tp berkas yg saya daftarkan tidak ada kejelasan katanya disuruh tunggu bulan
+            agustus
+            untuk mendaftarkan prona tahap 2 sedangkan tetangga saya sudah banyak yg mendapat sertifikat kemudian saya
+            coba kroscek
+            berkas ke basecamp ptsl yg ada di jln rajawali cipinang kata pak indra berkas saya tidak berjalan sedangkan
+            saya coba
+            cek ke basecamp ptsl yg ada di ruko dekat walikota berkas saya sudah ada nib nya tinggal tunggu kabar saja
+            (sudah pasti
+            jadi)"kata salah satu petugas disana" tapi bulan oktober 2019 kemarin saya coba kroscek lagi berkas saya
+            belum juga
+            berjalan kelangkah selanjutnya ,tolong beri kejelasan agar berkas saya dapat berjalan kembali dan segera di
+            cetak
+            sertifikatnya karena saya sudah menunggu 1tahun 9bulan</p>
+        <h5>Lampiran: <a style="text-align: right;">Waktu : 20-12-2019 20:20</a> <a href="">Lihat Selengkapnya ></a>
+            <hr>
+        </h5>
+    </div>
+
+
+</body>
+
+
+</html>
