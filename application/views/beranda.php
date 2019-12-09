970ac7e99400eadbfb9c910681057a77c7a271c4
NurAnggraheni <nanggraheni88@gmail.com>
2019-12-10 00:40:37 +0700

    Beranda

---
+++ application/views/beranda.php
@@ -0,0 +1,34 @@
+<!DOCTYPE htmml>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Lapor| Institut Teknologi SUmatera</title>	
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/style.css">
+</head> 
+<div class="header">
+                <div id="content">               
+                <button class="button">
+                    <a href="Tentang.html">Tentang Lapor</a>  
+                </button>
+                <button class="button">
+                    <a href="traveling.html">Masuk</a>  
+                </button>
+                <button class="button">
+                    <a href="food.html">Lapor</a>  
+                </button>
+                <button class="button">
+                    <a href="Tampilan1.php">Cari Aduan</a>  
+                </button>
+                </div>
+    </div> 
+    <div class="footer">
+         <h4>Download Aplikasi Lapor ITERA </h4>
+         <img class="foto" src="playstore.jpg" height="80px" width= "70px" align ="left" border="2"/>
+         <h4>Hubungi Kami</h4>
+         <img class="foto" src="email.png" height="80px" width= "70px" align ="left" border="2"/>
+         <img class="foto" src="fb.png" height="80px" width= "70px" align ="left" border="2"/>
+         <img class="foto" src="ig.png" height="80px" width= "70px" align ="left" border="2"/>
+
+        </div>
+</body>
+</html>
