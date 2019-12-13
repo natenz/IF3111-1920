a20f8dee9fe44fec60a28e7ad1a715e03cdc9345
Anggraheni <50392923+NurAnggraheni@users.noreply.github.com>
2019-12-13 15:02:24 +0700

    lapor
    lapor

---
+++ application/controllers/lapor.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Laporan ! LAPOR! </title>
+        <link rel="stylesheet" type="text/css" href=stylelapor.css>
+
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div id="menukiri">
+                  
+
+                </div>
+                <nav>
+                    <ul>
+                        <li class="active"><a href="#" > Tentang Lapor! </a></li>
+                        <li> <a href="#"> Laporan </a></li>
+                        <li> <a href="#"> Cari Aduan </a></li>
+                    
+                    </ul>
+                </nav>
+                <div id="menukanan">
+                    <nav>
+                        <ul>
+                            <li>
+                                <li> <a href="#"> Masuk </a></li> 
+                                <li> <a href="#"> Daftar </a></li>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            
+
+        </header>
+        <br><br><br><br><br><br><br><br>
+              
+       <section id="showcoment">
+            <div class="container">
+                    <div id="search-box">
+                            <form action="/search" id="search-form"method="get"target="_top">
+                                 <input id="search-text"name="keyword" placeholder="Search Here..."typr="text"/>
+                                 <button id="search-button"type="submit"name="cari">Search</button>
+                            </form>
+                    </div>
+                    <div class="create">
+                            <h4>Buat Laporan/Komentar
+                                <a href=""><img src="img/tambah.png" width="20px" height="20px" align="center"></a>
+                            </h4>
+                        </div>
+                        <div class="comment">
+                            <h5>Laporan/Komentar Terakhir
+                                <hr>
+                            </h5>
+                            <p> Saya salah satu warga kota tangerang no hp. 081411022970 ingin melaporkan kepada pemda
+                                kota tangerang (untuk diteruskan kepada pihak yang berwenang)tentang kondisi pohon besar
+                                dan tinggi yang berlokasi di jalan raden fatah tepatnya didepan sdn 01, 10, 12 sudimara barat
+                                dan lokasinya juga
+                            </p>
+                            <h5>Lampiran: <a style="text-align: right;">Waktu : 20-12-2019 20:20</a> <a href="">Lihat Selengkapnya ></a>
+                                <hr>
+                            </h5>
+                            <p>Selamat Siang,
+                                Saya ingin menanyakan perihal antrian paspor online, tanggal 09/11/2018 saya mendaftarkan akun saya melalui
+                                web, ketika
+                                saya mau mengakses lagi tahun ini, saya mencoba masuk via aplikasi "layanan paspor online" dan via website,
+                                email saya
+                                tidak dikenali dan dsuruh registrasi baru. Saat saya registrasi ulang, NIK saya telah dipakai.
+                                ketika saya lihat di internet, sarannya menggunakan NIK keluarga atau saudara yang kemungkinan tidak membuat
+                                paspor
+                                dalam waktu dekat. Apa tidak ada solusi lain? saya tidak ingin membuat saudara saya susah membuat paspor
+                                kedepannya
+                                nanti.
+                                bisa tolong bantuannya? saya akan pergi keluar bulan maret 2020.
+                                terimakasih banyak.</p>
+                            <h5>Lampiran: <a style="text-align: right;">Waktu : 20-12-2019 20:20</a> <a href="">Lihat Selengkapnya ></a>
+                                <hr>
+                            </h5>
+                            <p>Selamat sore saya ingin meminta kejelasan atas berkas yang saya daftarkan prona ptsl perseorangan bukan
+                                melalui rt
+                                setempat agustus 2018 di wilayah duren sawit rt 008/001,awal januari 2019 saya dapat kabar katanya akan ada
+                                pencetakan
+                                sertifikat di bulan maret 2019 tp berkas yg saya daftarkan tidak ada kejelasan katanya disuruh tunggu bulan
+                                agustus
+                                untuk mendaftarkan prona tahap 2 sedangkan tetangga saya sudah banyak yg mendapat sertifikat kemudian saya
+                                coba kroscek
+                                berkas ke basecamp ptsl yg ada di jln rajawali cipinang kata pak indra berkas saya tidak berjalan sedangkan
+                                saya coba
+                                cek ke basecamp ptsl yg ada di ruko dekat walikota berkas saya sudah ada nib nya tinggal tunggu kabar saja
+                                (sudah pasti
+                                jadi)"kata salah satu petugas disana" tapi bulan oktober 2019 kemarin saya coba kroscek lagi berkas saya
+                                belum juga
+                                berjalan kelangkah selanjutnya ,tolong beri kejelasan agar berkas saya dapat berjalan kembali dan segera di
+                                cetak
+                                sertifikatnya karena saya sudah menunggu 1tahun 9bulan</p>
+                            <h5>Lampiran: <a style="text-align: right;">Waktu : 20-12-2019 20:20</a> <a href="">Lihat Selengkapnya ></a>
+                                <hr>
+                            </h5>
+                        </div>
+                
+            </div> 
+
+        </section>
+        <footer>
+            <section id="spons">
+                <div class="container">
+                    <div class="box">
+                        <p>Download Aplikasi Mobile LAPOR!</p>
+                        <img src="img/google-play.png" width="150">
+    
+                    </div>
+                    <div class="box">
+                            <p>Dikembangkan Oleh : </p>
+                            <img src="img/itera.jpg" width="90" >
+                            
+        
+                     </div>
+                    <div class="box">
+                            <p>Lebih Dekat dengan Kami!</p>
+                            <img src="img/Instagram.png" width="49" >
+                            <img src="img/Facebook.png" width="52">
+        
+                        </div>
+                   
+                </div>
+            </section>
+            <p id="copyright" > Copyright  @2019</p>
+    
+        </footer>               
+           
+          
+  
+
+        
+    </body>
+
+        
+
+</html>
