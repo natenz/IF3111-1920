f76309134a16f3ce0f66fd4aa57bbad8457bed28
Anggraheni <50392923+NurAnggraheni@users.noreply.github.com>
2019-12-14 20:01:17 +0700

    controller tambah search
    controller tambah search

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -12,6 +12,12 @@ class Laporan extends CI_Controller
         $data=array('data'=>$this->Lapor_model->tampil_lapor());
         $this->load->view('lapor',$data);
     }
-
+    public function cari(){
+        $this->load->view('lapor');
+    }
+    public function search(){
+        $dicari=array('data'=>$this->lapor_model->get_cari());
+        $this->load->view('lapor',$dicari);
+    }
    
 }
